8bb9d589a199439041ce0539525b4d989ddacb8d
Jmack66 <mack.jonah@gmail.com>
2025-01-05 01:53:20 +0000

    init

---
+++ chemulink/CMakeFiles/simulink.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulink.dir/src/application.cpp.o"
+  "CMakeFiles/simulink.dir/src/application.cpp.o.d"
+  "simulink"
+  "simulink.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
